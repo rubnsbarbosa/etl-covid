2274c57ca80153ed4e84a09eee791767b29a34a2
rubnsbarbosa <rubensbarbosa90@gmail.com>
2020-10-02 03:59:00 -0300

    transforming

--- etl.py
+++ etl.py
@@ -22,3 +22,27 @@ def extracting():
     data = req.json()
     df = pd.DataFrame(data)
     return df
+
+def transforming(df):
+    logger.info('TRANSFORMING...')
+    logger.info('REMOVING COLUMNS')
+    list_drop_columns = ['estadoPaciente', 'codigoMunicipioPaciente', 'dataNotificacao', 'bairroPaciente', 'dataColetaExame',
+    'dataResultadoExame', 'resultadoFinalExame', 'dataSolicitacaoExame', 'dataObito', 'obitoConfirmado', 'paisPaciente']
+    df.drop(list_drop_columns, axis=1, inplace=True)
+
+    logger.info('SORTING THE DATE IN ASCENDING ORDER')
+    df_covid = df.sort_values(['dataInicioSintomas'], ascending=[1])
+
+    logger.info('NORMALIZING THE DATE')
+    df_covid['dataInicioSintomas'] = pd.to_datetime(df_covid['dataInicioSintomas'], utc=False)
+    df_covid['dataInicioSintomas'] = df_covid['dataInicioSintomas'].dt.date
+    print(df_covid[:5])
+
+    logger.info('FILTERING ON DATE')
+    df_covid = df_covid[df_covid['dataInicioSintomas'] == datetime.date.today()]
+
+    logger.info('CONVERT TYPES OF OBJECT TO NUMERIC AND DATETIME')
+    df_covid["idadePaciente"] = pd.to_numeric(df_covid["idadePaciente"])
+    df_covid["dataInicioSintomas"]= pd.to_datetime(df_covid["dataInicioSintomas"]) 
+    return df_covid
+
