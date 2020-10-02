ee2ae9d8c82a9c5172c6bd081c20afbbfcad7326
rubnsbarbosa <rubensbarbosa90@gmail.com>
2020-10-02 16:23:35 -0300

    cos config

---
+++ cos_config.py
@@ -0,0 +1,7 @@
+# Fake constants for IBM COS values
+cos = {
+  "apikey": "fake-apikey-b6ZTDS18TDMOoLgTyU",
+  "iam_serviceid_crn": "fake-serviceid-crn:v1:bluemix:public:iam-identity",
+  "endpoints": "https://control.cloud-object-storage.cloud.ibm.com/v2/endpoints",
+  "ibm_auth_endpoint": "https://iam.cloud.ibm.com/"
+}
