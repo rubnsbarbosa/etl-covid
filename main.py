469e29e3756e74003b1cd2a4d44792c01498e4ff
rubnsbarbosa <rubensbarbosa90@gmail.com>
2020-10-02 16:13:01 -0300

    main

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
+#!/usr/local/bin/python
 import pandas as pd
 from etl import extracting
 from etl import transforming
+from etl import loading
+
+import ibm_boto3
+import ibm_botocore.client
+from config import cos_client
 
 
 if __name__ == "__main__":
 
-    dataset = pd.DataFrame()
-    
     df = extracting()
-    df_covid = transforming(df)
-    print(df_covid)
+    data = transforming(df)
+
+    dataset = pd.DataFrame()
+    for idx, row in data.iterrows():
+        dataset = dataset.append(row, ignore_index=True)
+    # sava dataset in CSV file
+    dataset.to_csv("covid.csv", index=False)
+
+    loading(cos_client, 'event-bucket', 'covid.csv')
