ccdf1f8c2db9e500f07a055cfd094d2658684057
rubnsbarbosa <rubensbarbosa90@gmail.com>
2020-10-02 03:59:20 -0300

    main

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import pandas as pd
 from etl import extracting
+from etl import transforming
 
 
 if __name__ == "__main__":
+
+    dataset = pd.DataFrame()
     
     df = extracting()
-    print(df.head)
+    df_covid = transforming(df)
+    print(df_covid)
